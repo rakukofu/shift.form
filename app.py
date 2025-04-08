f4a509dee94f19cfdc390021b24901e245d38182
rakukofu <raku.kofu1234@gmail.com>
2025-04-08 14:33:26 +0900

    faviconのルートハンドラを追加

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify, send_from_directory
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -181,6 +181,11 @@ def logout():
     logout_user()
     flash('ログアウトしました。', 'success')
     return redirect(url_for('index'))
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                             'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.errorhandler(404)
 def not_found_error(error):
